1bd078609db507247f1fa85e0d2391a8e1967423
root <bahadiremre2744@gmail.com>
2019-07-16 15:59:07 +0000

    upload multi file

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express'),
 	app = express(),
-	  fs = require('fs');
-	  
+	fs = require('fs'),
+	multer = require('multer'),
+	storage = multer.diskStorage({
+		destination: function(req, file, callback) {
+			callback(null, 'public/images');
+		},
+		filename: function(req, file, callback) {
+			callback(null, file.fieldname + '-' + Date.now());
+		}
+	});
+
+var upload = multer({ storage: storage });
 
 app.use(express.static('public'));
 
@@ -14,13 +24,25 @@ app.get('/', (req, res) => {
 			res.render('index');
 		} else {
 			var imgFiles = files.filter(el => /\.jpg$/.test(el));
-			res.render('index', { images: imgFiles});
+			res.render('index', { images: imgFiles });
 		}
-	});	
+	});
 });
 
-app.get('/upload',(req,res)=>{
+app.get('/upload', (req, res) => {
 	res.render('upload');
+});
+
+app.post('/upload', upload.array('mypics', 12), (req, res) => {
+	const files = req.files;
+	if (!files) {
+		const error = new Error('Please choose files');
+		error.httpStatusCode = 400;
+		console.log(error);
+	}
+
+	// res.send(files);
+	res.redirect('/upload');
 });
 
 app.listen(3000, () => {
